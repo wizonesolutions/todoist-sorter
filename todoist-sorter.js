1896849c5954e6c46d0b5f04875d2345639a6f9f
Kevin Kaland <kevin@wizonesolutions.com>
2014-09-10 20:12:26 +0200

    Finish initial version.

--- todoist-sorter.js
+++ todoist-sorter.js
@@ -1,9 +1,24 @@
 var interval;
+var api;
 
 Meteor.startup(function () {
   if (Meteor.settings && Meteor.settings.todoistEmail && Meteor.settings.todoistPassword) {
-    checkForNewTasks();
-    interval = Meteor.setInterval(checkForNewTasks, (300000)); // every 5 minutes
+    console.log("Logging in...")
+    try {
+      api = new Todoist(Meteor.settings.todoistEmail, Meteor.settings.todoistPassword);
+      console.log("Logged in")
+    }
+    catch (err) {
+      console.log("Error");
+      console.log(err)
+    }
+    if (api.user) {
+      checkForNewTasks();
+      interval = Meteor.setInterval(checkForNewTasks, (300000)); // every 5 minutes
+    }
+    else {
+      console.log("WTF? Fix me pls.")
+    }
   }
 });
 
@@ -13,4 +28,78 @@ main = function (argv) {
 
 checkForNewTasks = function () {
   console.log("Checking for new tasks...");
+  // api.request('getUncompletedItems', { project_id: api.user.inbox_project }, function (err, res, data) {
+  api.request('query', { queries: JSON.stringify(['##']) }, function (err, res, data) {
+    if (err) {
+      console.log('Error checking for new tasks:');
+      console.log(err);
+    }
+    else {
+      data = data.data; // lol
+      if (! _.isEmpty(data)) {
+        api.request('getProjects', {}, function (err2, res2, data2) {
+          if (err2) {
+            console.log("Error getting projects");
+            console.log(err2);
+          }
+          else {
+            console.log("Finding ##project assignments...")
+
+            // Reduce to only inbox items.
+            data = _.where(data, { id: api.user.inbox_project });
+
+            _.each(data, function (item, index) {
+              var matches;
+              var search = /(.*)##([^\s]+)(.*)/;
+              if (matches = search.exec(item.content)) {
+                // console.log(item);
+
+                // What word actually matched?
+                projectName = matches[2];
+                console.log("Project name: " + projectName)
+
+                // OK great, now try to find a project with this name.
+                var project = _.findWhere(data2, { name: projectName });
+
+                if (project) {
+                  var projectId = project.id;
+
+                  console.log("Project ID: " + projectId)
+
+                  // Finally, move the item to this project. Then we are done.
+                  var projectMapping = {};
+                  projectMapping[item.project_id] = [item.id.toString()];
+                  api.request('moveItems', { project_items: JSON.stringify(projectMapping), to_project: projectId.toString() }, function (err3, res3, data3) {
+                    if (err3) {
+                      console.log("Problem moving task");
+                      console.log(err3);
+                    }
+                    else {
+                      console.log('Task "' + item.content + '" moved to project "' + project.name + '"');
+
+                      api.request('updateItem', { id: item.id, content: (matches[1] + matches[3]).trim() }, function (err4, res4, data4) {
+                        if (err4) {
+                          console.log("Couldn't fix task name");
+                          console.log(err4);
+                        }
+                        else {
+                          // Silence.
+                        }
+                      });
+                    }
+                  });
+                }
+                else {
+                  console.log("No matching project for " + matches[2])
+                }
+              }
+            });
+          }
+        });
+      }
+      else {
+        console.log("No data");
+      }
+    }
+  });
 }
